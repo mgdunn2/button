1a994c155c4ea26fd77f8d530328e0df3bfe514b
Michael Dunn <mgdunn@Michaels-MacBook-Air.local>
2018-03-03 21:55:30 -0500

    Remove hard coded credentials

--- brethttp.py
+++ brethttp.py
@@ -9,7 +9,6 @@ url = "https://www.wasbretontime.com/api/" + appkey + "/addtime"
 def send(time):
     data = dumps({'datetime': str(time)})
     http = Http()
-    #http.add_credentials("mike", "passwordwordpass")
     resp, content = http.request(uri=url,method='POST',headers={'Content-Type': 'application/json; charset=UTF-8'},body=data)
     if resp and 'status' in resp:
         return resp['status'] == '200'
