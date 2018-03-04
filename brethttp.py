9933ce16872c29e0caf9c9ecdbbd42a7742eac6a
Michael Dunn <mgdunn@Michaels-MacBook-Air.local>
2018-03-04 10:19:19 -0500

    Clean up send method

--- brethttp.py
+++ brethttp.py
@@ -1,15 +1,14 @@
 from httplib2 import Http
 from json import dumps, load
-from datetime import datetime
 
 confData = load(open('button.conf'))
 appkey = confData["APPKEY"]
 url = "https://www.wasbretontime.com/api/" + appkey + "/addtime"
 
 def send(time):
-    data = dumps({'datetime': str(time)})
-    http = Http()
-    resp, content = http.request(uri=url,method='POST',headers={'Content-Type': 'application/json; charset=UTF-8'},body=data)
-    if resp and 'status' in resp:
-        return resp['status'] == '200'
-    return False
+    resp, content = Http().request(
+        uri=url,
+        method='POST',
+        headers={'Content-Type': 'application/json; charset=UTF-8'},
+        body=dumps({'datetime': str(time)}))
+    return resp and 'status' in resp and resp['status'] == '200'
